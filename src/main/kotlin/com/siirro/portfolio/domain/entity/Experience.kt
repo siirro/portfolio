52859885c9aba199526865dbd862e317f8f78534
siirro <viostarryy@gmail.com>
2025-03-05 22:12:01 +0900

    feat: Entity 클래스 추가2

--- src/main/kotlin/com/siirro/portfolio/domain/entity/Experience.kt
+++ src/main/kotlin/com/siirro/portfolio/domain/entity/Experience.kt
@@ -3,11 +3,72 @@ package com.siirro.portfolio.domain.entity
 import jakarta.persistence.*
 
 @Entity
-class Experience : BaseEntity() {
+class Experience(
+    title: String,
+    description: String,
+    startYear: Int,
+    startMonth: Int,
+    endYear: Int?,
+    endMonth: Int?,
+    isActive: Boolean
+) : BaseEntity() {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "experience_id")
     var id: Long? = null
 
+    var title: String = title
+
+    var description: String = description
+
+    var startYear: Int = startYear
+
+    var startMonth: Int = startMonth
+
+    var endYear: Int? = endYear
+
+    var endMonth: Int? = endMonth
+
+    var isActive: Boolean = isActive
+
+    // mutableListOf() 쓰면 빈 리스트 만들어 줌
+    @OneToMany(
+        targetEntity = ExperienceDetail::class,
+        fetch = FetchType.LAZY,
+        cascade = [(CascadeType.ALL)]
+    )
+    @JoinColumn(name = "experience_id")
+    var details: MutableList<ExperienceDetail> = mutableListOf()
+
+    fun getEndYearMonth(): String {
+        if (endMonth == null || endMonth == null) {
+            return "present"
+        }
+        return "${endYear}.${endMonth}"
+    }
+
+    fun update(
+        title: String,
+        description: String,
+        startYear: Int,
+        startMonth: Int,
+        endYear: Int?,
+        endMonth: Int?,
+        isActive: Boolean
+    ) {
+        this.title = title
+        this.description = description
+        this.startYear = startYear
+        this.startMonth = startMonth
+        this.endYear = endYear
+        this.endMonth = endMonth
+        this.isActive = isActive
+    }
+
+    fun addDetails(detail: MutableList<ExperienceDetail>?) {
+        if (details != null) {
+            this.details.addAll(details)
+        }
+    }
 }
