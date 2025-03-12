6dae3b77615ac3ea90166b955a60b8cf14070ce9
siirro <viostarryy@gmail.com>
2025-03-12 22:54:25 +0900

    feat: 리포지토리 개발

--- src/main/kotlin/com/siirro/portfolio/presentation/repository/PresentationRepository.kt
+++ src/main/kotlin/com/siirro/portfolio/presentation/repository/PresentationRepository.kt
@@ -1,6 +1,39 @@
 package com.siirro.portfolio.presentation.repository
 
+import com.siirro.portfolio.domain.entity.*
+import com.siirro.portfolio.domain.repository.*
 import org.springframework.stereotype.Repository
 
 @Repository
-class PresentationRepository
+class PresentationRepository(
+    private val achievementRepository: AchievementRepository,
+    private val experienceRepository: ExperienceRepository,
+    private val introductionRepository: IntroductionRepository,
+    private val linkRepository: LinkRepository,
+    private val skillRepository: SkillRepository,
+    private val projectRepository: ProjectRepository
+) {
+    fun getActiveAchievements(): List<Achievement> {
+        return achievementRepository.findAllByIsActive(true)
+    }
+
+    fun getActiveExperiences(): List<Experience> {
+        return experienceRepository.findAllByIsActive(true)
+    }
+
+    fun getActiveIntroductions(): List<Introduction> {
+        return introductionRepository.findAllByIsActive(true)
+    }
+
+    fun getActiveLinks(): List<Link> {
+        return linkRepository.findAllByIsActive(true)
+    }
+
+    fun getActiveSkills(): List<Skill> {
+        return skillRepository.findAllByIsActive(true)
+    }
+
+    fun getActiveProjects(): List<Project> {
+        return projectRepository.findAllByIsActive(true)
+    }
+}
