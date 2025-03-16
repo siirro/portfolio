77feb56b4fb672b98c1b9c57f433df0b60e5e3a7
siirro <viostarryy@gmail.com>
2025-03-16 15:05:01 +0900

    feat: 인터셉터 개발

--- src/main/kotlin/com/siirro/portfolio/presentation/interceptor/PresentationInterceptor.kt
+++ src/main/kotlin/com/siirro/portfolio/presentation/interceptor/PresentationInterceptor.kt
@@ -1,6 +1,26 @@
 package com.siirro.portfolio.presentation.interceptor
 
+import com.siirro.portfolio.domain.entity.HttpInterface
+import com.siirro.portfolio.domain.repository.HttpInterfaceRepository
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
+import java.lang.Exception
 
 @Component
-class PresentationInterceptor
+class PresentationInterceptor(
+    private val httpInterfaceRepository: HttpInterfaceRepository
+) : HandlerInterceptor {
+
+    // pre는 컨트롤러 가기전, post는 컨트롤러 갔다온후(컨트롤러에서 예외받아오면 post는 발동x), after도 컨트롤러 갔다온후(예외가 발생해도 수행)
+    override fun afterCompletion(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        handler: Any,
+        ex: Exception?
+    ) {
+        val httpInterface = HttpInterface(request)
+        httpInterfaceRepository.save(httpInterface)
+    }
+}
